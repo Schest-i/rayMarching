0ad8f5757618cc89cac7c07f57fa4f5b9ad3e3c2
Schest <den.lomakin2018@gmail.com>
2023-04-24 22:12:45 +0300

    player coordinate fix

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Numerics;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,11 +12,11 @@ namespace ConsoleApp9
         {
             double alpha = Math.PI / 1800d;
             double radius = 5d;
-            double fov = 2* Math.PI / 3;
+            double fov = 2 * Math.PI / 3;
 
             int wallsize = 3;
            
-            Player player = new Player(6, 3 , Math.PI/3d);
+            Player player = new Player(6, 3, Math.PI / 3d);
 
             string[] map = { "*****#**********",
                              "****************",
@@ -41,7 +37,8 @@ namespace ConsoleApp9
                             };
             
 
-            Task.Factory.StartNew(() => {
+            Task.Factory.StartNew(() =>
+            {
                 ConsoleKeyInfo key = Console.ReadKey();
 
                 while (key.Key != ConsoleKey.Escape) 
@@ -59,49 +56,39 @@ namespace ConsoleApp9
             {
                 for (double i = player.rotation; i <= (fov + player.rotation); i += alpha)
                 {
-                    for (double j = 0; j <= radius; j+= 0.1d)
+                    for (double j = 0d; j <= radius; j += 0.1d)
                     {
-                        if (CheckWall(Math.Cos(i) * j, Math.Sin(i) * j, ref player, ref map)) WallDraw(i, j, wallsize, fov);
+                        try
+                        {
+                            if (CheckWall(i, j, player,ref map) == '#') WallDraw(i, j, wallsize, fov);
                     }
+                        catch{ }
+                        
                 }
-                Thread.Sleep(TimeSpan.FromSeconds(1/60));
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1 / 60));
                 Console.Clear();
             }
         }
-
-        private static void WallDraw(double i, double j, int wall, double fov)
+        internal static char CheckWall(double i, double j,Player player, ref string[] map) 
         {
-            int wallsize = (int)Math.Round(Math.Tan(fov/2)*j * wall, 0);
-            try
-            {
+            return map[(int)Math.Round(Math.Sin(i) * j, 0) - player.y][(int)Math.Round(Math.Cos(i) * j, 0) + player.x];
+        }
+
+        internal static void WallDraw(double i, double j, int wall, double fov)
+        {
+            int wallsize = (int)Math.Round(Math.Tan(fov / 2) * j * wall, 0);
+
                 for (int k = 0; k < wallsize; k++)
                 {
 
-                    int left = (int)Math.Abs(Math.Round(Console.WindowWidth * i / fov, 0));
-                    int top = (int)Math.Abs(Math.Round(Console.WindowHeight / 2d + wallsize / 2d - k, 0));
-                    Console.SetCursorPosition( left, top);
+                int left = (int)Math.Round(Console.WindowWidth * i / fov, 0);
+                int top = (int)Math.Round(Console.WindowHeight / 2d + wallsize / 2d - k, 0);
+                Console.SetCursorPosition(left, top);
                     Console.Write("#");
                 }           
             }
-            catch { }
-        }
-
-        static internal bool CheckWall(double x, double y,ref Player player, ref string[] map) 
-        {
-            
-            int iY = (int)Math.Round(y, 0);
-            int iX = (int)Math.Round(x, 0);
-            try
-            {
-                return map[iY + player.y][iX + player.x] == '#';
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-        }
-
     }
-}
+        }
+
 
